db0aa53d9240ce93d560c458eb8b64bf11382326
Kapil Patwa <kapil.p@peopleinteractive.in>
2020-04-26 19:39:41 +0530

    completed ex3-raindops

---
+++ raindrops/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn raindrops(n: u32) -> String {
+    let mut str = String::from("");
+    if n % 3 == 0 { 
+        str.push_str("Pling")
+    } 
+    if n % 5 == 0 {
+        str.push_str("Plang")
+    }
+    if n % 7 == 0 {
+        str.push_str("Plong")
+    }
+    if str.len() == 0 {
+        str = n.to_string();
+    }
+    str
+}
