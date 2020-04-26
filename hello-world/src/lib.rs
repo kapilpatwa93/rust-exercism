4fc493bbaf91189689c107262a66716c4be67838
Kapil Patwa <kapil.p@peopleinteractive.in>
2020-04-26 13:25:55 +0530

    completed ex1

---
+++ hello-world/src/lib.rs
@@ -0,0 +1,6 @@
+// The &'static here means the return type has a static lifetime.
+// This is a Rust feature that you don't need to worry about now.
+pub fn hello() -> String {
+    String::from("Hello, World!")
+}
+
