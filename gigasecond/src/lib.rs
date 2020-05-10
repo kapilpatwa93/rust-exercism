4e788bbb3723ca0fe8cc378d28e121327ff023e4
Kapil Patwa <kapil.p@peopleinteractive.in>
2020-05-10 20:09:25 +0530

    completed ex13:gigaseconds

---
+++ gigasecond/src/lib.rs
@@ -0,0 +1,6 @@
+use chrono::{DateTime, Utc, Duration};
+
+// Returns a Utc DateTime one billion seconds after start.
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    start +  Duration::seconds((10 as i64).pow(9))
+}
